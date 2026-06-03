e26623281e9b3d8d29d873d938fa3221bac4f17e
Matthew <lojsamatvej@gmail.com>
2026-06-03 16:51:28 +0300

    add JWT authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+     if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({
+            code: 401,
+            status: 'failure',
+            message: 'Access denied. No token provided.'
+        });
+    }
+    const token = authHeader.split(' ')[1] //* token position
+    jwt.verify(
+        token,
+        process.env.JWT_SECRET,
+        (err, decoded) =>{
+            if (err) {
+            return res.status(401).json({
+                code: 401,
+                status: 'failure',
+                message: 'Invalid token.'
+            });
+        }
+        
+        req.user = decoded;
+        next();
+    });
+
+}
+
+module.exports = verifyJWT;
