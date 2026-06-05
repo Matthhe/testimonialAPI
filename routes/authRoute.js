b5b6e18abc4964e881a9ccebffad48438765acf0
Matthew <lojsamatvej@gmail.com>
2026-06-05 12:35:32 +0300

    add rate limiting to auth endpoints

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const authController = require('../controllers/authController')
+const limiter = require('../middleware/rateLimitter')
 
-router.post('/login', authController.Login)
-router.post('/register', authController.Register)
+router.post('/login', limiter, authController.Login)
+router.post('/register', limiter, authController.Register)
 
 module.exports = router
